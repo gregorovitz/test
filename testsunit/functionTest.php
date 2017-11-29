426c9d122b376adbdfea416fa554de13d79606b8
gregorovitz <emanumarque@gmail.com>
2017-11-29 18:42:09 +0100

    test

--- testsunit/functionTest.php
+++ testsunit/functionTest.php
@@ -11,8 +11,8 @@ require 'includes/function.php'
 class functionTest extends TestCase{
 	
 	public function testverif_pwd(){
-		private $mdp='pomme';
-		private $hashedPwd ;
+		$mdp='pomme';
+		$hashedPwd ;
 		$hashedPwd = password_hash($mdp, PASSWORD_DEFAULT);
 		$value =verif_pwd($mdp,$hashedPwd);
 		assertTrue($value);
